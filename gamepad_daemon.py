c9f3a5341a08a3a5142a368a8baf71587ec1ac6e
Thuto <thutolepheana9039@gmail.com>
2026-05-17 08:19:13 +0200

    update: added a final button for a 6 button + joystick setup

--- gamepad_daemon.py
+++ gamepad_daemon.py
@@ -5,11 +5,12 @@ from evdev import AbsInfo, UInput, ecodes
 
 capabilities = {
     ecodes.EV_KEY: [
-        ecodes.BTN_SELECT,  # Joy Click (Coin)
+        ecodes.BTN_SELECT,  # Joystick Click (Coin)
         ecodes.BTN_START,  # SunFounder (Start)
-        ecodes.BTN_SOUTH,  # G1 ('A' / Shoot)
-        ecodes.BTN_EAST,  # G2 ('B' / Jump)
-        ecodes.BTN_NORTH,  # G3 ('X' / Grenade)
+        ecodes.BTN_SOUTH,  # G1 (A)
+        ecodes.BTN_EAST,  # G2 (B)
+        ecodes.BTN_NORTH,  # G3 (X)
+        ecodes.BTN_WEST,  # G4 (Y)
     ],
     ecodes.EV_ABS: [
         (
@@ -24,7 +25,7 @@ capabilities = {
 }
 
 try:
-    gamepad = UInput(capabilities, name="RP2040 Arcade Pro", version=0x6)
+    gamepad = UInput(capabilities, name="RP2040 NeoGeo Controller", version=0x8)
     arduino = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
 except Exception as e:
     print(f"Startup Error: {e}")
@@ -38,20 +39,27 @@ try:
             raw_line = arduino.readline().decode("utf-8").strip()
             data = raw_line.split(",")
 
-            if len(data) == 7:
+            # Expecting exactly 8 data points
+            if len(data) == 8:
                 try:
                     x_val, y_val = int(data[0]), int(data[1])
-                    joy_b, start_b = int(data[2]), int(data[3])
-                    g1_b, g2_b, g3_b = int(data[4]), int(data[5]), int(data[6])
+                    sel_b, start_b = int(data[2]), int(data[3])
+                    a_b, b_b, x_b, y_b = (
+                        int(data[4]),
+                        int(data[5]),
+                        int(data[6]),
+                        int(data[7]),
+                    )
 
                     gamepad.write(ecodes.EV_ABS, ecodes.ABS_X, x_val)
                     gamepad.write(ecodes.EV_ABS, ecodes.ABS_Y, y_val)
 
-                    gamepad.write(ecodes.EV_KEY, ecodes.BTN_SELECT, joy_b)
+                    gamepad.write(ecodes.EV_KEY, ecodes.BTN_SELECT, sel_b)
                     gamepad.write(ecodes.EV_KEY, ecodes.BTN_START, start_b)
-                    gamepad.write(ecodes.EV_KEY, ecodes.BTN_SOUTH, g1_b)
-                    gamepad.write(ecodes.EV_KEY, ecodes.BTN_EAST, g2_b)
-                    gamepad.write(ecodes.EV_KEY, ecodes.BTN_NORTH, g3_b)
+                    gamepad.write(ecodes.EV_KEY, ecodes.BTN_SOUTH, a_b)
+                    gamepad.write(ecodes.EV_KEY, ecodes.BTN_EAST, b_b)
+                    gamepad.write(ecodes.EV_KEY, ecodes.BTN_NORTH, x_b)
+                    gamepad.write(ecodes.EV_KEY, ecodes.BTN_WEST, y_b)
 
                     gamepad.syn()
 
